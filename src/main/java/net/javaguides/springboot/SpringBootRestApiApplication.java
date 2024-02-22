7c3c1bf707beb56002c53d886fb8f4fcbe0e4291
rvdxk <radek.szczot@gmail.com>
2024-02-22 18:32:57 +0100

    added bean and controller classes

--- src/main/java/net/javaguides/springboot/SpringBootRestApiApplication.java
+++ src/main/java/net/javaguides/springboot/SpringBootRestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootRestApiApplication {
+public class SpringBootRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRestApiApplication.class, args);
+		SpringApplication.run(SpringBootRestApiApplication.class, args);
 	}
 
 }
